b465b94b89c9279b2d532ce0d9369b58bb3819a8
ThiThamKim <kimtham97@gmail.com>
2023-11-09 11:03:24 +0100

    3 nouvelles classes Héritant de Console (Portable,Salon,Hybride) et passer la classe mère en abstract

---
+++ NintendoProject/src/main/java/model/Hybride.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Hybride extends Console {
+
+	public Hybride(String nom, double prix, String dateSortie) {
+		super(nom, prix, dateSortie);
+		// TODO Auto-generated constructor stub
+	}
+
+}
